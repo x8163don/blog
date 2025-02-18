5d0a1da35e128bb22e3913dc783fe2b0f32e9345
Bevis <x8163don@gmail.com>
2025-02-18 11:49:05 +0800

    feat: change redirect rule

--- astro.config.ts
+++ astro.config.ts
@@ -24,7 +24,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 export default defineConfig({
   output: 'static',
   redirects:{
-    '/':'/blog/blog'
+    '/':'/blog/'
   },
   integrations: [
     tailwind({
